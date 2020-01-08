7842190c7659f53ba9f7c5816fa97662e372d6be
Jop <meijer.jop@gmail.com>
2020-01-08 14:28:44 +0100

    nieuw main repository

---
+++ code/classes/netlist.py
@@ -0,0 +1,88 @@
+import csv
+
+
+class Netlist():
+    def __init__(self, print_nr, netlist_nr):
+        self.gates = {}
+        self.path = {}
+        max_x = 0
+        max_y = 0
+
+        # map x, y coordinates of chips
+        with open(f'gates&netlists/chip_{print_nr}/print_{print_nr}.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for chip, x, y in reader:
+                try:
+                    self.gates[int(chip)] = (int(x), int(y))
+
+                    # set boundaries of grid
+                    if int(x) > max_x:
+                       max_x = int(x)
+                    if int(y) > max_y:
+                        max_y = int(y) 
+                except ValueError:
+                    pass
+            
+        self.boundaries = ((0, 0), (max_x + 1, max_y + 1))
+        print(self.boundaries)
+        
+        self.connections = []
+        with open(f'gates&netlists/chip_{print_nr}/netlist_{netlist_nr}.csv', newline='') as csvfile:
+            netlist = csv.reader(csvfile)
+            for chip_a, chip_b in netlist:
+                try:
+                    manhattan_distance = abs(self.gates[int(chip_b)][0] - self.gates[int(chip_a)][0]) + abs(self.gates[int(chip_b)][1] - self.gates[int(chip_a)][1])
+                    self.connections.append((int(chip_a), int(chip_b), manhattan_distance))
+                except ValueError:
+                    pass
+            self.connections.sort(key=lambda connection: connection[2])
+        print(self.connections)
+        print()
+
+
+    def connect(self, connection):
+        self.path[connection] = []
+        chip_a = connection[0]
+        chip_b = connection[1]
+        print(connection)
+        x_a = self.gates[chip_a][0]
+        y_a = self.gates[chip_a][1]
+        x_b = self.gates[chip_b][0]
+        y_b = self.gates[chip_b][1]
+
+        print(f"chip a: {x_a}, {y_a}\nchip_b: {x_b}, {y_b}")
+
+        diff_x = x_b - x_a
+        diff_y = y_b - y_a
+
+        while x_a != x_b or y_a != y_b:
+
+            if diff_x < 0:
+                if not self.check_for_chip((x_a - 1, y_a)):
+                    x_a -= 1
+
+            elif diff_x > 0:
+                self.check_for_chip((x_a + 1, y_a))
+                x_a += 1       
+            elif diff_y < 0:
+                self.check_for_chip((x_a, y_a - 1))
+                y_a -= 1
+            elif diff_y > 0:
+                self.check_for_chip((x_a, y_a + 1))
+                y_a += 1
+
+            self.path[connection].append((x_a, y_a))
+
+        print(self.path[connection])
+
+
+    def check_for_chip(self, next_coor):
+        for key in self.gates:
+            if next_coor in self.gates[key]:
+                return True
+        return False
+
+    def check_for_right_chip(self, next_coor, destination):
+        if next_coor == destination:
+            return True
+        return False
---
+++ main.py
@@ -0,0 +1,32 @@
+import csv
+import os, sys
+from sys import argv
+directory = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(directory, "code"))
+sys.path.append(os.path.join(directory, "code", "classes"))
+sys.path.append(os.path.join(directory, "code", "algoritmes"))
+
+from netlist import Netlist
+
+
+def main():
+    print_nr = int(input("Print_nr: "))
+    netlist_nr = int(input("Netlist_nr: "))
+    print()
+    print()
+    
+    netlist = Netlist(print_nr, netlist_nr)
+    print(netlist.gates)
+    for connection in netlist.connections:
+        print()
+        netlist.connect(connection)
+        print()
+
+
+
+if __name__ == "__main__":
+    main()
+    
+
+    
+    
