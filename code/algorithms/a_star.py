9c8291e256de5de0c50ff1f66a0b705bf64392ab
Navisa Rajabali <navisarajabali@wcw-stud-145-109-41-15.wireless.uva.nl>
2020-01-27 14:01:17 +0100

    changes hillclimber

--- code/algorithms/a_star.py
+++ code/algorithms/a_star.py
@@ -1,29 +1,28 @@
-from code.classes.netlist import *
-from code.algorithms.helpers import * 
+"""
+a_star.py
 
+A*-algorithm for pathfinding between coordinates for given netlist.
+Returns Boolean function.
+
+(C) 2020 Teamname, Amsterdam, The Netherlands
+"""
+
+from code.classes.netlist import *
+from code.algorithms.helpers import *
 
 def a_star(netlist):
-    """
-    A*-algorithm for pathfinding between coordinates
-
-    Input: 
-    Netlist object to perform A*-algorithm on
-
-    Returns: 
-    Boolean; True if solution is found
-             False otherwise
-    """
+    """A*-algorithm for pathfinding between coordinates"""
 
     # hardcoded list of all possible directions (north, east, south, west, up, down)
     directions = [(-1, 0, 0), (0, -1, 0), (0, 0, -1), (1, 0, 0), (0, 1, 0), (0, 0, 1)]
-    
+
     # iterate over all connections in netlist
-    for connection in netlist.netlist:        
-        
+    for connection in netlist.netlist:
+
         # coordinates of chip_a and chip_b
         chip_a = connection[0]
         chip_b = connection[1]
-        
+
         # chip coordinates split into x-, y-, z- coordinates
         x_a = netlist.print.chips[chip_a][0]
         y_a = netlist.print.chips[chip_a][1]
@@ -33,14 +32,15 @@ def a_star(netlist):
         y_b = netlist.print.chips[chip_b][1]
         z_b = netlist.print.chips[chip_b][2]
 
-
+        # define boundaries of grid
         min_x = netlist.print.boundaries[0][0]
         max_x = netlist.print.boundaries[1][0]
-        min_y = netlist.print.boundaries[0][1] 
+        min_y = netlist.print.boundaries[0][1]
         max_y = netlist.print.boundaries[1][1]
         min_z = netlist.print.boundaries[0][2]
         max_z = netlist.print.boundaries[1][2]
 
+        # define origin and destination
         origin = (x_a, y_a, z_a)
         current_coordinate = origin
         destination = (x_b, y_b, z_b)
@@ -49,14 +49,14 @@ def a_star(netlist):
         priorities = []
         paths = {}
         blocked_coordinate = (0, 0, 0)
-     
+
         # perform pathfinding until the destination coordinate is reached
         while x_a != x_b or y_a != y_b or z_a != z_b:
             current_coordinate = (x_a, y_a, z_a)
 
             # iterate over all possible directions
             for direction in directions:
-                
+
                 # create temporary coordinates
                 temp_x_a = x_a + direction[0]
                 temp_y_a = y_a + direction[1]
@@ -66,22 +66,21 @@ def a_star(netlist):
 
                 # calculate manhattan distance to destination
                 cost = abs(x_b - temp_x_a) + abs(y_b - temp_y_a) + abs(z_b - temp_z_a)
-                
-                
+
                 # verify that temporary coordinates are valid coordinates
                 if ((not netlist.check_if_path(temp_coordinate) or netlist.check_if_chip(temp_coordinate))
                      and not temp_coordinate in paths and not (temp_coordinate, cost) in priorities
                      and ((netlist.check_if_chip(temp_coordinate) and temp_coordinate == destination) or not netlist.check_if_chip(temp_coordinate))
-                     and (not temp_x_a < min_x and not temp_x_a > max_x 
+                     and (not temp_x_a < min_x and not temp_x_a > max_x
                           and not temp_y_a < min_y and not temp_y_a > max_y
                           and not temp_z_a < min_z and not temp_z_a > max_z)):
-               
+
                     # relate new coordinate to old coordinate for tracing
                     paths[temp_coordinate] = current_coordinate
 
                     # assign cost penalty if coordinate is close to a wrong chip
                     if netlist.penalty(temp_coordinate, origin, destination):
-                        cost += 1                
+                        cost += 1
 
                     # adjust cost for heuristic values
                     if direction == (0, 0, 1):
@@ -96,23 +95,23 @@ def a_star(netlist):
             priorities.sort(key=lambda coordinate: coordinate[1])
 
             # set new x-, y-, z- coordinates if there are valid coordinates to go to
-            if len(priorities) != 0:                 
+            if len(priorities) != 0:
                 x_a = priorities[0][0][0]
                 y_a = priorities[0][0][1]
                 z_a = priorities.pop(0)[0][2]
-            
+
             # remove all succesful paths, move failed connection to front of netlist
-            else:                
+            else:
                 netlist.clear()
-                netlist.netlist.insert(0, netlist.netlist.pop(netlist.netlist.index(connection)))               
+                netlist.netlist.insert(0, netlist.netlist.pop(netlist.netlist.index(connection)))
                 return False
-                
+
         # trace route from destination to origin
         netlist.path[connection] = trace(paths, (x_a, y_a, z_a))
-        
+
         # convert path coordinates to x-, y-, z- coordinate lists for visualization via matplotlib
-        netlist.path_plot[connection]  = matlib_convert(netlist.path[connection])           
-    
+        netlist.path_plot[connection]  = matlib_convert(netlist.path[connection])
+
     # verify that all connections have been made
     if netlist.test():
 
